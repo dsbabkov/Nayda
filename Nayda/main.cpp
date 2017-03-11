b672841e5d2293501fb6813d3ab6ea08e1eb86ac
Mar94oK <w.uvarenkov@gmail.com>
2017-03-11 18:11:12 +0300

    Added QDebug support

--- Nayda/main.cpp
+++ Nayda/main.cpp
@@ -1,11 +1,15 @@
 #include "main_application.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Main_Application w;
     w.show();
+
+
+    qDebug() << "Here we go!";
 
     return a.exec();
 }
